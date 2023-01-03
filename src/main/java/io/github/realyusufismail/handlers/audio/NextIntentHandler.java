34629b45d73507a405862a648456fd795ae44d70
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-01-03 18:37:45 +0000

    Finished with next

--- src/main/java/io/github/realyusufismail/handlers/audio/NextIntentHandler.java
+++ src/main/java/io/github/realyusufismail/handlers/audio/NextIntentHandler.java
@@ -18,9 +18,14 @@
  */ 
 package io.github.realyusufismail.handlers.audio;
 
+import static io.github.realyusufismail.utils.Utils.getAudioPlayerState;
+
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.interfaces.audioplayer.PlayBehavior;
+import io.github.realyusufismail.reciters.Reciter;
+import io.github.realyusufismail.utils.QuranUtils;
 import java.util.Optional;
 
 public class NextIntentHandler implements RequestHandler {
@@ -31,6 +36,52 @@ public class NextIntentHandler implements RequestHandler {
 
   @Override
   public Optional<Response> handle(HandlerInput handlerInput) {
-    return Optional.empty();
+    var currenSurahNumber =
+        handlerInput.getAttributesManager().getSessionAttributes().get("surahNumber");
+    if (currenSurahNumber == null) {
+      handlerInput
+          .getResponseBuilder()
+          .withSpeech(
+              "You haven't started listening to any surah yet. Please start listening to a surah first.")
+          .withShouldEndSession(false);
+    }
+
+    var newSurahNumber = Integer.parseInt((String) currenSurahNumber) + 1;
+
+    if (newSurahNumber > 114) {
+      handlerInput
+          .getResponseBuilder()
+          .withSpeech(
+              "You have reached the end of the Quran. Please start listening to a surah first.")
+          .withShouldEndSession(false);
+    }
+
+    handlerInput
+        .getAttributesManager()
+        .getSessionAttributes()
+        .put("surahNumber", String.valueOf(newSurahNumber));
+
+    final var newSurahName = QuranUtils.getSuraName(newSurahNumber);
+
+    var reciter =
+        (Reciter) handlerInput.getAttributesManager().getSessionAttributes().get("reciter");
+
+    if (reciter == null) {
+      reciter = Reciter.DEFAULT_RECITER;
+    }
+
+    final var surahUrl = reciter.getSurahUrl(newSurahNumber);
+
+    var audioPlayer = getAudioPlayerState(handlerInput);
+    var token = audioPlayer.getToken();
+    var offsetInMilliseconds = audioPlayer.getOffsetInMilliseconds();
+
+    return handlerInput
+        .getResponseBuilder()
+        .withSpeech("Playing surah " + newSurahName + " from " + reciter.getName())
+        .withShouldEndSession(false)
+        .addAudioPlayerPlayDirective(
+            PlayBehavior.REPLACE_ALL, offsetInMilliseconds, "", token, surahUrl)
+        .build();
   }
 }
